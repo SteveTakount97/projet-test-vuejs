618d4e312d06e509fc5b9bbdebec476417952f46
steve <stevetak@outlook.fr>
2025-01-10 01:53:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Bonjour</h1>
+  <p>comment allez-vous</p>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Styles de Vuetify
+import '@mdi/font/css/materialdesignicons.css'; // Fichier CSS des icônes
+
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'light', 
+  },
+});
+
+const app = createApp(App);
+
+app.use(vuetify); // Ajout de Vuetify à l'application
+app.mount('#app');
