bae4b222adab76caad4540971c1f28b9e823471b
steve <stevetak@outlook.fr>
2025-01-10 04:14:02 +0100

    initial home page

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
-  <h1>Bonjour</h1>
-  <p>comment allez-vous</p>
+  <v-container class="container">
+    <header>
+      <h1>ARSENE</h1>
+      <p>votre coach numérique</p>
+    </header>
+    <v-footer>
+      <v-row class="justify-center">
+        <v-col class="text-center">
+          <span>© 2025 ARSENE</span>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-container>
 </template>
+
+<style>
+ @import './assets/main.css';
+</style>
